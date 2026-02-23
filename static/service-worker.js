187cfef4e13937b062f7e20f72a50d37ffb4f752
neguebaop <neguebaop5@gmail.com>
2026-02-23 18:15:28 -0300

    PWA: base.html + manifest + service worker + icons

---
+++ static/service-worker.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = "linkflix-v1";
+const URLS_TO_CACHE = ["/", "/login", "/static/styles.css"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
